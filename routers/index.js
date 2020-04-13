2dee15555ea54f4425cacd250fd6ed500d566cad
yuan xu <a2589654@gmail.com>
2020-04-13 15:05:21 -0700

    role endpoints and tested

--- routers/index.js
+++ routers/index.js
@@ -92,5 +92,43 @@ router.get('/manage/user/list', (req, res) => {
         });
 });
 
+router.post('/manage/role/add', (req, res) => {
+    const {roleName} = req.body;
+    RoleModel.create({name: roleName})
+        .then(role => {
+            res.send({status: 0, data: role})
+        })
+        .catch(error => {
+            console.error('create role error', error);
+            res.send({status: 1, msg: 'Failed to create role, please try again'});
+        });
+});
+
+router.get('/manage/role/list', (req, res) => {
+    RoleModel.find()
+        .then(roles => {
+            res.send({status: 0, data: roles});
+        })
+        .catch(error => {
+            console.error('get role list error', error);
+            res.send({status: 1, msg: 'Failed to get role list, please try again'});
+        });
+});
+
+router.post('/manage/role/update', (req, res) => {
+    const role = req.body;
+    role.auth_time = Date.now();
+    RoleModel.findOneAndUpdate({_id: role._id}, role)
+        .then(oldRole => {
+            // console.log('---', oldRole._doc)
+            res.send({status: 0, data: {...oldRole._doc, ...role}});
+        })
+        .catch(error => {
+            console.error('update role error', error);
+            res.send({status: 1, msg: 'Failed to update role, please try again'});
+        });
+});
+
+
 module.exports = router;
 
